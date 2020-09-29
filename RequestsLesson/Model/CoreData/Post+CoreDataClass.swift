29d30f6d494f097bcafa052a58aae561e8153c06
marselles1 <55791765+marselles1@users.noreply.github.com>
2020-09-29 09:54:11 +0300

    Update Post+CoreDataClass.swift

--- RequestsLesson/Model/CoreData/Post+CoreDataClass.swift
+++ RequestsLesson/Model/CoreData/Post+CoreDataClass.swift
@@ -2,8 +2,7 @@
 //  Post+CoreDataClass.swift
 //  RequestsLesson
 //
-//  Created by Евгений on 30.12.2019.
-//  Copyright © 2019 Ildar Zalyalov. All rights reserved.
+//  Created by marsel on 28.08.2020.
 //
 //
 
